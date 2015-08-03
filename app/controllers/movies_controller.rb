58ea50700c015bd3554830a97cb64752db2b4509
Rachelle Herrera <rachelle.herera@gmail.com>
2015-08-03 16:21:17 -0700

    created a new form for new movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,41 @@
 class MoviesController < ApplicationController
-
-  def index
+  def index 
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.new
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create 
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else 
+      render 'new'
+    end
+  end
+
+  def edit 
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:success] = 'Movie updated'
+      redirect_To movies_path
+    else
+      render 'edit'
+    end 
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :summary)
+  end
 end
