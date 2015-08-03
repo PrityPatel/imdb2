a918a61017ede2f4dc04d4b0a0112630cf3d8988
Prity Patel <pritypatel714@gmail.com>
2015-08-03 16:57:16 -0700

    resolved merge conflicts

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,12 +1,9 @@
 class Actor < ActiveRecord::Base
-<<<<<<< HEAD
 
   def age
     @actor = Actor.find_by_id(id)
     Time.now.year - @actor.year_of_birth
   end
 
-=======
   has_and_belongs_to_many :movies
->>>>>>> 6d9c9821d89f46415ec9f681643d44cb11711250
 end
